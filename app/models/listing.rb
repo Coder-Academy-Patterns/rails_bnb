2850e1836f8a205f992d8697a2e0c7187524da33
Patrick Smith <patrick@burntcaramel.com>
2017-10-12 10:53:37 +1100

    Add seeded listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,13 @@
 class Listing < ApplicationRecord
+  validates :title, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :country_code, presence: true
+  validates :country, presence: {
+    message: -> (listing, data) { "No country for code: #{listing.country_code}" }
+  }
+  validates :bed_count, numericality: { greater_than: 0 }
+
   geocoded_by :full_address
   after_validation :geocode
 
@@ -7,6 +16,7 @@ class Listing < ApplicationRecord
   end
 
   def full_address
+    return nil if country.nil?
     "#{street_address}, #{city}, #{country.name}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.create!([
+  { title: 'White House', street_address: '1600 Pennsylvania Avenue NW', city: 'Washington, DC', country_code: 'us', bed_count: rand(1..20) },
+  { title: 'Eiffel Tower', street_address: 'Champ de Mars, 5 Avenue Anatole France', city: 'Paris', country_code: 'fr', bed_count: rand(1..20) },
+  { title: 'Big Ben', street_address: 'Big Ben, Westminster', city: 'London', country_code: 'gb', bed_count: rand(1..20) },
+  { title: 'Eureka Tower', street_address: '7 Riverside Quay', city: 'Southbank', country_code: 'au', bed_count: rand(1..20), night_fee_cents: rand(2000..20000), cleaning_fee_cents: rand(1000..10000) },
+  { title: 'Burj Khalifa', street_address: '1 Sheikh Mohammed bin Rashid Blvd', city: 'Dubai', country_code: 'ae', bed_count: rand(1..20), night_fee_cents: rand(2000..20000), cleaning_fee_cents: rand(1000..10000) }
+])
